986936dc74000540f5d8e1920f622a74d94cabcc
Christopher Renaud Oelerich <chris@oeleri.ch>
2024-06-18 15:28:53 -0500

    update repo

--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
 apk add --no-cache git
 
 
-if [ -z "$(ls -A "$REPO")" ]; then
+if [ -n "$(ls -A "/$REPO")" ]; then
+  cd /"$REPO"
+  if git remote -v | grep origin | grep -q "https://github.com/$ORG/$REPO.git"; then
+    echo "Repo unchanged"
+  else
+    rm -r * .*
+  cd /
+  fi
+fi
+
+if [ -z "$(ls -A /"$REPO")" ]; then
   git clone https://github.com/"$ORG"/"$REPO".git 
 fi
 
-cd "$REPO"
+cd /"$REPO"
 
 python3 -m http.server "$PORT"
 
